6344431df88da8cdae65447b1e81ca3954e4f2c0
Luke Anderson <stuntguy3000@gmail.com>
2015-11-26 12:25:17 +1030

    Fix typo

--- src/main/java/me/stuntguy3000/java/redditlivebot/RedditLiveBot.java
+++ src/main/java/me/stuntguy3000/java/redditlivebot/RedditLiveBot.java
@@ -75,7 +75,7 @@ public class RedditLiveBot {
                     continue;
                 }
                 default: {
-                    LogHandler.log("Unknown command! Commands: count, stoplive, stop, botfather1");
+                    LogHandler.log("Unknown command! Commands: count, stoplive, stop, botfather");
                 }
             }
         }
