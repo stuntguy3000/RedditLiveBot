f47fb2e7a5c377cf16a615ed43fc26ccfc5553b4
Amir Omidi <amir@aaomidi.com>
2016-01-02 02:26:02 +0330

    Check permissions for admin messages.

--- src/main/java/me/stuntguy3000/java/redditlivebot/handler/CommandHandler.java
+++ src/main/java/me/stuntguy3000/java/redditlivebot/handler/CommandHandler.java
@@ -1,7 +1,10 @@
 package me.stuntguy3000.java.redditlivebot.handler;
 
+import me.stuntguy3000.java.redditlivebot.RedditLiveBot;
 import me.stuntguy3000.java.redditlivebot.model.TelegramCommand;
+import me.stuntguy3000.java.redditlivebot.util.BotSettings;
 import pro.zackpollard.telegrambot.api.event.chat.message.CommandMessageReceivedEvent;
+import pro.zackpollard.telegrambot.api.user.User;
 
 import java.util.HashMap;
 
@@ -31,12 +34,19 @@ public class CommandHandler {
     public void executeCommand(String s, CommandMessageReceivedEvent event) {
         s = s.toLowerCase();
         TelegramCommand cmd = null;
+        BotSettings botSettings = RedditLiveBot.getInstance().getConfigHandler().getBotSettings();
+        User user = event.getMessage().getSender();
 
         if (s.equals("admin")) {
             if (event.getArgs().length == 0) {
                 // Send help message.
                 return;
             }
+            if (!botSettings.getTelegramAdmins().contains(user.getId())) {
+                // Send a no permission message.
+                return;
+            }
+
             cmd = adminCommands.get(event.getArgs()[0].toLowerCase());
         } else {
             cmd = normalCommands.get(s);
