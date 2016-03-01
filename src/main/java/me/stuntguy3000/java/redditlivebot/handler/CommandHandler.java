8eb2703255b0342be3c13448c69417b4e39bf83f
Luke Anderson <stuntguy3000@gmail.com>
2016-03-02 08:51:19 +1030

    Command debugging

--- src/main/java/me/stuntguy3000/java/redditlivebot/handler/CommandHandler.java
+++ src/main/java/me/stuntguy3000/java/redditlivebot/handler/CommandHandler.java
@@ -3,6 +3,7 @@ package me.stuntguy3000.java.redditlivebot.handler;
 import me.stuntguy3000.java.redditlivebot.RedditLiveBot;
 import me.stuntguy3000.java.redditlivebot.model.TelegramCommand;
 import me.stuntguy3000.java.redditlivebot.util.BotSettings;
+import me.stuntguy3000.java.redditlivebot.util.LogHandler;
 import pro.zackpollard.telegrambot.api.event.chat.message.CommandMessageReceivedEvent;
 import pro.zackpollard.telegrambot.api.user.User;
 
@@ -12,24 +13,12 @@ import java.util.HashMap;
  * Created by amir on 2015-11-25.
  */
 public class CommandHandler {
-    public HashMap<String, TelegramCommand> normalCommands;
     public HashMap<String, TelegramCommand> adminCommands;
+    public HashMap<String, TelegramCommand> normalCommands;
 
     public CommandHandler() {
         normalCommands = new HashMap<>();
         adminCommands = new HashMap<>();
-    }
-
-    public void registerCommand(TelegramCommand cmd) {
-        switch (cmd.getCommandType()) {
-
-            case NORMAL:
-                normalCommands.put(cmd.getName().toLowerCase(), cmd);
-                break;
-            case ADMIN:
-                adminCommands.put(cmd.getName().toLowerCase(), cmd);
-                break;
-        }
     }
 
     public void executeCommand(String s, CommandMessageReceivedEvent event) {
@@ -45,6 +34,7 @@ public class CommandHandler {
             }
             if (!botSettings.getTelegramAdmins().contains(user.getId())) {
                 // Send a no permission message.
+                LogHandler.log("User %s (%s) tried to run an admin command!", user.getId(), user.getUsername());
                 return;
             }
 
@@ -53,7 +43,10 @@ public class CommandHandler {
             cmd = normalCommands.get(s);
         }
 
-        if (cmd == null) return;
+        if (cmd == null) {
+            LogHandler.log("Command %s is unknown!", s);
+            return;
+        }
         cmd.processCommand(event);
     }
 
@@ -66,5 +59,17 @@ public class CommandHandler {
         }
 
         return sb.toString();
+    }
+
+    public void registerCommand(TelegramCommand cmd) {
+        switch (cmd.getCommandType()) {
+
+            case NORMAL:
+                normalCommands.put(cmd.getName().toLowerCase(), cmd);
+                break;
+            case ADMIN:
+                adminCommands.put(cmd.getName().toLowerCase(), cmd);
+                break;
+        }
     }
 }
