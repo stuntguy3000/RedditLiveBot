00142d51540c19164f89f85bb6f4e1d8151d48cd
Luke Anderson <stuntguy3000@gmail.com>
2016-03-23 09:57:44 +1030

    Fix version command

--- src/main/java/me/stuntguy3000/java/redditlivebot/command/normal/VersionCommand.java
+++ src/main/java/me/stuntguy3000/java/redditlivebot/command/normal/VersionCommand.java
@@ -3,6 +3,8 @@ package me.stuntguy3000.java.redditlivebot.command.normal;
 import me.stuntguy3000.java.redditlivebot.RedditLiveBot;
 import me.stuntguy3000.java.redditlivebot.model.commandtypes.TelegramNormalCommand;
 import pro.zackpollard.telegrambot.api.chat.Chat;
+import pro.zackpollard.telegrambot.api.chat.message.send.ParseMode;
+import pro.zackpollard.telegrambot.api.chat.message.send.SendableTextMessage;
 import pro.zackpollard.telegrambot.api.event.chat.message.CommandMessageReceivedEvent;
 
 // @author Luke Anderson | stuntguy3000
@@ -14,10 +16,11 @@ public class VersionCommand extends TelegramNormalCommand {
     public void processCommand(CommandMessageReceivedEvent event) {
         Chat chat = event.getChat();
 
-        respond(chat, "*RedditLive by* @stuntguy3000\n" +
+        respond(chat, SendableTextMessage.builder().message("*RedditLive by* @stuntguy3000\n" +
                 "`Build " + RedditLiveBot.BUILD + "`\n\n" +
                 "Source [Available on GitHub](https://github.com/stuntguy3000/RedditLiveBot)\n" +
-                "Created using @zackpollard's [JavaTelegramBotAPI](https://github.com/zackpollard/JavaTelegramBot-API)");
+                "Created using @zackpollard's [JavaTelegramBotAPI](https://github.com/zackpollard/JavaTelegramBot-API)")
+                .parseMode(ParseMode.MARKDOWN).disableWebPagePreview(true).build());
     }
 }
     
