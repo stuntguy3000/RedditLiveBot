fed69d97d3f375b9d7cd825f6b3afccb7a092e4e
Luke Anderson <stuntguy3000@gmail.com>
2016-04-06 09:48:31 +0930

    Debug

--- src/main/java/me/stuntguy3000/java/redditlivebot/scheduler/LiveThreadTask.java
+++ src/main/java/me/stuntguy3000/java/redditlivebot/scheduler/LiveThreadTask.java
@@ -62,12 +62,9 @@ public class LiveThreadTask extends TimerTask {
             for (LiveThreadChildren liveThreadChild : liveThread.getData().getChildren()) {
                 LiveThreadChildrenData data = liveThreadChild.getData();
 
-                Lang.sendDebug("LP: %s Data: %s Compare: %s", lastPost.getId(), data.getId(), data.getId().equals(lastPost.getId()));
                 if (lastPost == null || !data.getId().equals(lastPost.getId())) {
-                    Lang.sendDebug("1");
                     updates.add(data);
                 } else {
-                    Lang.sendDebug("2");
                     break;
                 }
             }
