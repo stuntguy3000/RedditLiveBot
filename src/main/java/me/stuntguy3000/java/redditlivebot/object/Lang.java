8413bd327e995212d0526c90b523d603c85721b8
Luke Anderson <stuntguy3000@gmail.com>
2016-04-20 09:49:35 +0930

    Correct sanitation

--- src/main/java/me/stuntguy3000/java/redditlivebot/object/Lang.java
+++ src/main/java/me/stuntguy3000/java/redditlivebot/object/Lang.java
@@ -94,9 +94,9 @@ public class Lang {
     }
 
     public static void sendHtml(Chat chat, String message, String... format) {
-        Object[] newFormatters = new String[format.length - 1];
+        Object[] newFormatters = new Object[format.length - 1];
 
-        for (int i = 0; i < format.length; i++) {
+        for (int i = 0; i < format.length - 1; i++) {
             newFormatters[i] = format[i].replace("<", "&lt;").replace(">", "&gt;").replace("&", "&amp;");
         }
 
